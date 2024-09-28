54a12ed2f243fe511f9442493ce1229b0c15e188
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-29 01:50:31 +0300

    feat: add data fetching states for event participants

--- frontend/src/pages/EventParticipantsPage/EventParticipantsPage.jsx
+++ frontend/src/pages/EventParticipantsPage/EventParticipantsPage.jsx
@@ -5,29 +5,46 @@ import axios from '~/services/axios';
 
 import { PageTitle } from '~/components/common/PageTitle/PageTitle.styled';
 import { ParticipantList } from '~/components/ParticipantList/ParticipantList';
+import { PlaceholderText } from '~/components/common/Placeholder/Placeholder.styled';
+import { Loader } from '~/components/common/Loader/Loader';
 
 const EventParticipantsPage = () => {
   const { eventId } = useParams();
   const { state } = useLocation();
   const [participants, setParticipants] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
+        setError(false);
+        setIsLoading(true);
+
         const { data } = await axios.get(`/participants/event/${eventId}`);
         setParticipants(data);
-      } catch {
-        //
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     })();
   }, [eventId]);
 
+  const loading = !error && isLoading;
+  const hasError = !isLoading && error;
+  const content = !error && participants.length > 0;
+  const noData = !isLoading && !error && participants.length === 0;
+
   return (
     <div>
       <PageTitle>{`"${state?.eventTitle}"`} participants</PageTitle>
-      {participants.length > 0 && (
-        <ParticipantList participants={participants} />
+      {content && <ParticipantList participants={participants} />}
+      {loading && <Loader />}
+      {hasError && (
+        <PlaceholderText>Oops.. Something went wrong</PlaceholderText>
       )}
+      {noData && <PlaceholderText>No Participants</PlaceholderText>}
     </div>
   );
 };
