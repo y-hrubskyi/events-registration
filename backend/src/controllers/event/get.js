d863866512769ba05934c81f98d31de849c58886
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 18:10:29 +0300

    feat: add pagination for get events request

--- backend/src/controllers/event/get.js
+++ backend/src/controllers/event/get.js
@@ -2,7 +2,9 @@ import { eventService } from '#services/index.js';
 
 export const get = async (req, res, next) => {
   try {
-    const events = await eventService.get();
+    const { page = 1, limit = 12 } = req.query;
+
+    const events = await eventService.get(page, limit);
     res.json(events);
   } catch (error) {
     next(error);
