6268324f2b8c6d184f8f5fa0ab307b8f61c52a56
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 23:18:15 +0300

    feat: add fetching events (without render)

--- frontend/src/pages/EventsBoardPage/EventsBoardPage.jsx
+++ frontend/src/pages/EventsBoardPage/EventsBoardPage.jsx
@@ -1,5 +1,29 @@
+import { useEffect, useState } from 'react';
+
+import axios from '~/services/axios';
+
 const EventsBoardPage = () => {
-  return <div>EventsBoardPage</div>;
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get('/events?');
+        setEvents(data.events);
+      } catch {
+        //
+      }
+    })();
+  }, []);
+
+  const isEmpty = events.length === 0;
+
+  return (
+    <div>
+      Events
+      {isEmpty ? <p>No Data</p> : <p>Done</p>}
+    </div>
+  );
 };
 
 export default EventsBoardPage;
