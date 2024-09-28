16435224b0f0009f5f9d9f6dc4b5a10c121ec1fc
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 23:21:48 +0300

    feat: add data fetching states for events

--- frontend/src/pages/EventsBoardPage/EventsBoardPage.jsx
+++ frontend/src/pages/EventsBoardPage/EventsBoardPage.jsx
@@ -1,27 +1,46 @@
 import { useEffect, useState } from 'react';
 
+import { Loader } from '~/components/common/Loader/Loader.styled';
+import { PlaceholderText } from '~/components/common/Placeholder/Placeholder.styled';
+
 import axios from '~/services/axios';
 
 const EventsBoardPage = () => {
   const [events, setEvents] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     (async () => {
       try {
-        const { data } = await axios.get('/events?');
+        setError(false);
+        setIsLoading(true);
+
+        const { data } = await axios.get('/events');
+
         setEvents(data.events);
-      } catch {
-        //
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setIsLoading(false);
       }
     })();
   }, []);
 
-  const isEmpty = events.length === 0;
+  const isEmpty = !error && !isLoading && events.length === 0;
 
   return (
     <div>
       Events
-      {isEmpty ? <p>No Data</p> : <p>Done</p>}
+      {isLoading && <Loader />}
+      {error && !isLoading && (
+        <PlaceholderText>Oops.. Something went wrong</PlaceholderText>
+      )}
+      {isEmpty ? (
+        <PlaceholderText>No Data</PlaceholderText>
+      ) : (
+        <PlaceholderText>Done</PlaceholderText>
+      )}
     </div>
   );
 };
