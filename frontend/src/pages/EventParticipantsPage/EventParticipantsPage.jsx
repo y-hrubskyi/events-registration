2d82956373133a11fad88315c385c8a740ce8510
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-29 01:49:38 +0300

    feat: add fetching event participants (without fetching states)

--- frontend/src/pages/EventParticipantsPage/EventParticipantsPage.jsx
+++ frontend/src/pages/EventParticipantsPage/EventParticipantsPage.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from 'react';
+import { useLocation, useParams } from 'react-router-dom';
+
+import axios from '~/services/axios';
+
+import { PageTitle } from '~/components/common/PageTitle/PageTitle.styled';
+import { ParticipantList } from '~/components/ParticipantList/ParticipantList';
+
 const EventParticipantsPage = () => {
-  return <div>EventParticipantsPage</div>;
+  const { eventId } = useParams();
+  const { state } = useLocation();
+  const [participants, setParticipants] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get(`/participants/event/${eventId}`);
+        setParticipants(data);
+      } catch {
+        //
+      }
+    })();
+  }, [eventId]);
+
+  return (
+    <div>
+      <PageTitle>{`"${state?.eventTitle}"`} participants</PageTitle>
+      {participants.length > 0 && (
+        <ParticipantList participants={participants} />
+      )}
+    </div>
+  );
 };
 
 export default EventParticipantsPage;
