10b038ac960b3140832fe0073ebde7cad6c8f533
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 15:15:50 +0300

    feat: init backend start
     - create server (app)
     - create & add middlewares
     - create start IIFE function (run connectDb & startServer)

---
+++ backend/src/middlewares/errorHandler.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+export const errorHandler = (err, req, res, next) => {
+  const { status = 500, message = 'Server error', code, name } = err;
+
+  if (name === 'MongoServerError' && code === 11000) {
+    return res.status(409).json({ message });
+  }
+
+  if (name === 'ValidationError') {
+    return res.status(400).json({ message });
+  }
+
+  res.status(status).json({ message });
+};
