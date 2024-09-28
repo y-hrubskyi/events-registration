e6cefb53e50b4505d343e13dddcd989ec8149627
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 20:44:35 +0300

    feat: create routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,27 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+
+import SharedLayout from '~/components/SharedLayout/SharedLayout';
+import EventsBoardPage from '~/pages/EventsBoardPage/EventsBoardPage';
+import EventRegistrationPage from '~/pages/EventRegistrationPage/EventRegistrationPage';
+import EventParticipantsPage from '~/pages/EventParticipantsPage/EventParticipantsPage';
+
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Navigate to="/events" />} />
+        <Route path="events" element={<EventsBoardPage />} />
+        <Route
+          path="events/:eventId/registration"
+          element={<EventRegistrationPage />}
+        />
+        <Route
+          path="events/:eventId/participants"
+          element={<EventParticipantsPage />}
+        />
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Route>
+    </Routes>
+  );
 };
