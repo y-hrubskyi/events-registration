024ac4307d11b44959e36d3098078d5590a93478
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 19:34:33 +0300

    feat: expand participantRoutes

--- backend/src/routes/participantRoutes.js
+++ backend/src/routes/participantRoutes.js
@@ -6,4 +6,6 @@ const router = Router();
 
 router.get('/event/:eventId', participantController.getByEvent);
 
+router.post('/', participantController.create);
+
 export { router as participantRoutes };
