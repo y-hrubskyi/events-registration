f365990e2b59317372f256f0b2f3b8e828c7de0c
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 21:46:24 +0300

    feat: add global styles using createGlobaleStyle, connect fonts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App.jsx';
+import { GlobalStyle } from '~/styles/GlobalStyle.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </StrictMode>
