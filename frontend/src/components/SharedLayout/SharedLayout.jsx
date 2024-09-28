abcb9e32f553acb91540139aee66a76867863084
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-28 20:52:36 +0300

    feat: add basic navigation

--- frontend/src/components/SharedLayout/SharedLayout.jsx
+++ frontend/src/components/SharedLayout/SharedLayout.jsx
@@ -1,10 +1,18 @@
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 export const SharedLayout = () => {
   return (
-    <div>
-      SharedLayout
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 20
+      }}
+    >
+      <Link to="/">Events Board</Link>
+
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
