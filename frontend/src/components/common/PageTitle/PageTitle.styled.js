d7df5b6155b51bdbb65838b48a1ba65d4fa259f6
y-hrubskyi <y.hrubskyi@gmail.com>
2024-09-30 11:02:23 +0300

    refactor: rename PageTitle > BasePageTItle, create PageTitle

--- frontend/src/components/common/PageTitle/PageTitle.styled.js
+++ frontend/src/components/common/PageTitle/PageTitle.styled.js
@@ -1,9 +1,13 @@
 import styled from 'styled-components';
 
-export const PageTitle = styled.h1`
+export const BasePageTitle = styled.h1`
   margin-bottom: ${p => p.theme.spacing(10)};
   font-size: 24px;
   font-weight: 600;
   text-align: center;
   color: ${p => p.theme.colors.black};
+`;
+
+export const PageTitle = styled(BasePageTitle)`
+  margin-bottom: ${p => p.theme.spacing(5)};
 `;
